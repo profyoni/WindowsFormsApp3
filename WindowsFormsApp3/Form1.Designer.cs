c1abe2f99d36d0e2dab04432a4156111332c09b0
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2020-12-28 20:41:43 -0500

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 408);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 42);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStr";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(237, 32);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // avoid tight loops that use CPU...
+        // for example when polling a service, sleep between repeated polls
+        private void LongTaskLikeGettingWebPageOrWebService(string s)
+        {
+            toolStripStatusLabel1.Text = @"Starting Processing";
+            Thread.Sleep(4000); // 100ms
+            toolStripStatusLabel1.Text = @"Finished Processing";
+        }
+
+        
+        private void button1_Clickv1(object sender, EventArgs e)
+        {
+           Task.Run( () => LongTaskLikeGettingWebPageOrWebService(e.ToString()));
+        }
+
+        // Downsides of new Threads
+        // 1. resource intensive for OS
+        // 2. may introduce thread contention issues (synchronization) = race conditions
+
+
+        // async and await
+        // uses same thread as invoked
+        // hides the complexity of multi threading
+
+        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/
+        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/task-asynchronous-programming-model
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "" + GetUrlContentLengthAsync();
+        }
+
+        public async Task<int> GetUrlContentLengthAsync()
+        {
+            toolStripStatusLabel1.Text = @"Starting Processing";
+            var client = new HttpClient();
+
+            Task<string> getStringTask =
+                client.GetStringAsync("https://docs.microsoft.com/dotnet");
+
+            DoIndependentWork();
+
+            string contents = await getStringTask;
+            Trace.WriteLine(contents);
+            toolStripStatusLabel1.Text = @"Ending Processing";
+            return contents.Length;
+        }
+
+        void DoIndependentWork()
+        {
+            Console.WriteLine("Working...");
+        }
+    }
+}
